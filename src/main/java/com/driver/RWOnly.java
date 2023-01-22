30ed5d19b31a6efbabfd17c157b9d9a4a4733bde
AashishMahant7 <imthebestrimthe@gmail.com>
2023-01-22 19:41:25 +0530

    Encapsulation

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int aashish;
+
+    public int getAashish() {
+        return 7;
+    }
+
+    public void setAashish(int aashish) {
+        this.aashish = 7;
+    }
 }
